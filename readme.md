2b5f71fbec285883a363e506e3b48f2549e4df05
Elio Struyf <info@estruyf.be>
2016-09-26 16:53:08 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,73 @@
+# Sample Node.js SharePoint webhook application
+
+This is a sample webhook application built with the use of Node.js. Feel free to explore.
+
+![Notification sample](./assets/notifications.png)
+
+## Azure AD application and certificate configuration
+To test out this sample, you will have to create a service application on Azure AD. 
+
+> For more information you can read this article: [Building daemon or service app with the Microsoft Graph API](https://www.eliostruyf.com/building-daemon-or-service-app-with-the-microsoft-graph-api/). Instead of defining Microsoft Graph permissions, you will need to configure it for SharePoint Online instead.
+
+![Read & Write permissions](./assets/azure-ad-permissions.png)
+
+In the linked article you can read how to do it for a C# application. This can be done a bit differently for Node.js. The Azure AD application creation stays the same.
+
+- Run `$ npm install -g keycred`
+- Run `$ keycred`
+    - Fill in the questions
+- Once you provided all the info, your certificate gets created:
+![Certificate information](./assets/certificate.png)
+
+- Add the flowing information in your Azure AD manifest file:
+    - customKeyIdentifier
+    - value
+    - keyId
+
+```JSON
+"keyCredentials": [
+    {
+        "customKeyIdentifier": "customKeyIdentifier",
+        "keyId": "keyId",
+        "type": "AsymmetricX509Cert",
+        "usage": "Verify",
+        "value":  "value"
+    }
+],
+```
+
+- In your folder, create a new **privatekey.pem** file and paste in the private key information:
+
+```
+-----BEGIN RSA PRIVATE KEY-----
+THE KEY ITSELF
+-----END RSA PRIVATE KEY-----
+```
+
+- Add the fingerPrint ID to the config.json file (check configuration section)
+
+## Installation
+Follow the next steps if you want to test it out:
+- Clone this repo
+- Open your command prompt and navigate to the folder
+- Add your configuration to the config.json file (you can use the sample file)
+    - adalConfig
+        - authority: change the tenant name
+        - clientId: specify your Azure AD client application ID
+        - subscriptionUrl: the URL that SharePoint has to call. Use ngrok for testing.
+        - resouce: the URL of your SharePoint site for which you want to retrieve the access token
+        - fingerPrint: finger print of your certificate
+    - webhookConfig
+        - url: URL of the SharePoint site
+        - listName: name of the list / library to which you want to subscribe your webhook
+        - clientState: clientState name you want to use for your subscription
+- Run: `$ npm install`
+- Run: `$ tsd install`
+
+**During development:** 
+- open another command prompt. Navigate to the ngrok folder and run:*
+    - `$ ngrok http 3000`
+    - copy the **https** forwarding URL of ngrok and use it in the config.json file for the **subscriptionUrl**
+- Run: `$ npm start`
+    - This transpiles TypeScript to JavaScript and start the server on http://localhost:3000
+- Navigate to http://localhost:3000/create which automatically creates a new subscription for the config you provided
---
+++ src/routes/home.ts
@@ -0,0 +1,77 @@
+import * as express from "express";
+import * as request from 'request';
+import * as moment from 'moment';
+
+import TokenHelper from '../helpers/TokenHelper';
+import ChangeHelper from '../helpers/ChangeHelper';
+import { IConfig } from '../Interfaces/IConfig';
+
+const config: IConfig = require('../../config.json');
+
+export default class HomeRoutes {
+    private router = express.Router();
+    private tokenHelper: TokenHelper;
+    private changeHelper: ChangeHelper;
+
+    constructor() {
+        this.tokenHelper = new TokenHelper();
+        this.changeHelper = new ChangeHelper(config);
+    }
+
+    public routes(): express.Router {
+        this.router.get('/', (req: express.Request, res: express.Response) => {
+            res.send('Go to <a href="http://localhost:3000/create">http://localhost:3000/create</a>');
+            res.status(200);
+        });
+
+        // Listen route for the webhook
+        this.router.post('/listen', (req: any, res: express.Response) => {
+            if (req.query && req.query.validationtoken) {
+                console.log('Found validation token: ', req.query.validationtoken);
+                res.send(req.query.validationtoken);
+                res.status(200);
+            } else {
+                this.changeHelper.check(req.body).then((data) => {
+                    console.log(data);
+                    res.sendStatus(200);
+                }).catch((err) => {
+                    res.sendStatus(400);
+                });
+            }
+        });
+
+        // Create a new subscription
+        this.router.get('/create', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+            this.tokenHelper.getAppOnlyAccessToken(config).then((token) => {
+                request({
+                    method: "POST",
+                    uri: `${config.webhookConfig.url}/_api/web/lists/getbytitle('${config.webhookConfig.listName}')/subscriptions`,
+                    headers: {
+                        'Authorization': 'Bearer ' + token,
+                        'Accept': 'application/json;odata=verbose',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        "resource": `${config.webhookConfig.url}/_api/web/lists/getbytitle('${config.webhookConfig.listName}')`,
+                        "notificationUrl": config.adalConfig.subscriptionUrl,
+                        "expirationDateTime": moment().add(90, 'days'),
+                        "clientState": config.webhookConfig.clientState
+                    })
+                }, (error, resp, body) => {
+                    if (error !== null) {
+                        console.log('ERROR:', error);
+                        res.send(error);
+                        res.status(400);
+                        return;
+                    }
+
+                    console.log('Body:', body);
+                    res.send(body);
+                    res.status(200);
+                });
+            });
+        });
+
+        return this.router;
+    }
+}
