fd05701ed04035dc223cafd83847238bccca4eda
Elio Struyf <info@estruyf.be>
2016-09-26 21:35:56 +0200

    List and delete end-point added

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -24,6 +24,60 @@ export default class HomeRoutes {
             res.status(200);
         });
 
+        // List all the subscriptions on the list
+        this.router.get('/list', (req: express.Request, res: express.Response) => {
+            this.tokenHelper.getAppOnlyAccessToken(config).then((token) => {
+                request({
+                    uri: `${config.webhookConfig.url}/_api/web/lists/getbytitle('${config.webhookConfig.listName}')/subscriptions`,
+                    headers: {
+                        'Authorization': 'Bearer ' + token,
+                        'Accept': 'application/json;odata=nometadata',
+                        'Content-Type': 'application/json'
+                    }
+                }, (error, resp, body) => {
+                    if (error !== null) {
+                        console.log('ERROR:', error);
+                        res.send(error);
+                        res.status(400);
+                        return;
+                    }
+
+                    console.log('Body:', body);
+                    res.send(body);
+                    res.status(200);
+                });
+            });
+        });
+
+        // Delete a subscription
+        this.router.get('/delete/:subId', (req: any, res: express.Response) => {
+            var subId = req.params.subId;
+            if (subId !== null) {
+                this.tokenHelper.getAppOnlyAccessToken(config).then((token) => {
+                    request({
+                        method: 'DELETE',
+                        uri: `${config.webhookConfig.url}/_api/web/lists/getbytitle('${config.webhookConfig.listName}')/subscriptions('${subId}')`,
+                        headers: {
+                            'Authorization': 'Bearer ' + token,
+                            'Accept': 'application/json;odata=nometadata',
+                            'Content-Type': 'application/json'
+                        }
+                    }, (error, resp, body) => {
+                        if (error !== null) {
+                            console.log('ERROR:', error);
+                            res.send(error);
+                            res.status(400);
+                            return;
+                        }
+
+                        res.sendStatus(200);
+                    });
+                });
+            } else {
+                res.sendStatus(400);
+            }
+        });
+
         // Listen route for the webhook
         this.router.post('/listen', (req: any, res: express.Response) => {
             if (req.query && req.query.validationtoken) {
